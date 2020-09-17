1e0811cf7189b242219c3f211350ac681e65e00b
daign <jakob.mischek@daign.de>
2020-09-17 21:26:37 +0200

    added unit test for SvgRenderer

---
+++ test/svgRenderer.spec.ts
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+
+import { MockDocument, MockNode } from '@daign/mock-dom';
+import { View } from '@daign/2d-pipeline';
+import { GraphicStyle, Line } from '@daign/2d-graphics';
+import { StyleSheet } from '@daign/style-sheets';
+
+import { SvgRenderer } from '../lib/svgRenderer';
+
+declare var global: any;
+
+describe( 'SvgRenderer', (): void => {
+  beforeEach( (): void => {
+    global.document = new MockDocument();
+  } );
+
+  describe( 'render', (): void => {
+    it( 'should append the rendered node to the target node', (): void => {
+      // Arrange
+      const styleSheet = new StyleSheet<GraphicStyle>();
+      const svgRenderer = new SvgRenderer( styleSheet );
+      const node = new Line();
+      const view = new View();
+      view.mountNode( node );
+      const target = new MockNode();
+
+      // Act
+      svgRenderer.render( view, target );
+
+      // Assert
+      expect( target.children.length ).to.equal( 1 );
+    } );
+  } );
+} );
